df187d9d7e56ebf2b106422cb6596da857705a25
Kyle9901 <2249994784@qq.com>
2026-06-10 10:27:34 +0800

    修复ros2 run失败问题

--- setup.py
+++ setup.py
@@ -1,8 +1,49 @@
 import os
 from glob import glob
 from setuptools import setup
+import setuptools.command.install
+import setuptools.command.develop
 
 package_name = 'vision_grasp'
+
+
+def _create_lib_symlinks(install_base):
+    """Create symlinks in lib/<package_name>/ pointing to bin/ executables."""
+    bin_dir = os.path.join(install_base, 'bin')
+    lib_pkg_dir = os.path.join(install_base, 'lib', package_name)
+    if os.path.isdir(bin_dir):
+        os.makedirs(lib_pkg_dir, exist_ok=True)
+        for fname in os.listdir(bin_dir):
+            src = os.path.join(bin_dir, fname)
+            dst = os.path.join(lib_pkg_dir, fname)
+            if os.path.isfile(src) and not os.path.exists(dst):
+                os.symlink(src, dst)
+
+
+class CustomInstall(setuptools.command.install.install):
+    """Custom install command that creates lib/<package_name>/ symlinks for ros2 run."""
+
+    def run(self):
+        super().run()
+        _create_lib_symlinks(self.install_base)
+
+
+class CustomDevelop(setuptools.command.develop.develop):
+    """Custom develop command that creates lib/<package_name>/ symlinks for ros2 run."""
+
+    def run(self):
+        super().run()
+        # In develop mode, install_dir is the site-packages directory.
+        # Derive the package install prefix from COLCON_PREFIX_PATH.
+        install_base = None
+        colcon_prefix = os.environ.get('COLCON_PREFIX_PATH', '')
+        if colcon_prefix:
+            # COLCON_PREFIX_PATH may contain multiple paths; use the first one.
+            prefix = colcon_prefix.split(':')[0]
+            install_base = os.path.join(prefix, package_name)
+        if install_base and os.path.isdir(install_base):
+            _create_lib_symlinks(install_base)
+
 
 setup(
     name=package_name,
@@ -16,7 +57,7 @@ setup(
             glob(os.path.join('launch', '*.launch.py'))),
     ],
     install_requires=['setuptools'],
-    zip_safe=True,
+    zip_safe=False,
     maintainer='Alkaid',
     maintainer_email='alkaid@example.com',
     description='Vision-based grasping for AGX Arm',
@@ -28,5 +69,9 @@ setup(
             'grasp_executor = vision_grasp.grasp_executor:main',
             'test_move_to = vision_grasp.test_move_to:main',
         ],
+    },
+    cmdclass={
+        'install': CustomInstall,
+        'develop': CustomDevelop,
     },
 )
